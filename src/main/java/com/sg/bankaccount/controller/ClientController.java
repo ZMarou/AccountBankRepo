6dd90adc4d2f16d980d337e8d1f05dc354203069
zmarou <zaag.m@sfeir.com>
2019-10-10 11:12:21 +0200

    [fix] fix cross origin problem on localhost

--- src/main/java/com/sg/bankaccount/controller/ClientController.java
+++ src/main/java/com/sg/bankaccount/controller/ClientController.java
@@ -3,6 +3,7 @@ package com.sg.bankaccount.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(path = "/api/client")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ClientController {
 
 	@Autowired
